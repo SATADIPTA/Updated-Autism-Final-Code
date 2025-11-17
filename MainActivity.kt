00117060db8e4e040c0c66eb76f4a8c669a5fb28
SATADIPTA <145426685+SATADIPTA@users.noreply.github.com>
2025-11-17 16:33:11 +0530

    Implement MainActivity.kt
    Implement MainActivity with edge-to-edge support

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.firebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.firebase.ui.theme.FirebaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val authViewModel : AuthViewModel by viewModels()
+        setContent {
+            FirebaseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyAppNavigation(modifier = Modifier.padding(innerPadding), authViewModel = authViewModel)
+
+                }
+            }
+        }
+    }
+}
